a77bb79e1bd72979b5ff250a9e3f6a3b4eb5856f
DevScissors <sean.scissors@gmail.com>
2018-05-31 09:46:51 -0700

    Added JS30 files and updated JS folder

---
+++ js/navigation.js
@@ -0,0 +1,20 @@
+// Add active class to the current Nav Link (highlight it)  
+var navContainer = document.getElementsByClassName("topNav");
+var navLinks = navContainer.getElementsByClassName("navLink")
+
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
